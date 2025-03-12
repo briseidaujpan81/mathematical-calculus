02dcc8562bdb6b22beb870e8438562e09d0dd262
briseidaujpan81 <briseidaujpan81@gmail.com>
2025-03-12 07:20:28 +0000

    Add 7.c

---
+++ src/7.c
@@ -0,0 +1,11 @@
+int main() {
+  int a = 5;
+  float b = 3.14;
+  double c = 2.78;
+
+  printf("The value of a is %d\n", a);
+  printf("The value of b is %f\n", b);
+  printf("The value of c is %lf\n", c);
+
+  return 0;
+}
