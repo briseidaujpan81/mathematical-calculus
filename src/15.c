2d89055fef790399e1a0c3ed55e187067de21e17
briseidaujpan81 <briseidaujpan81@gmail.com>
2025-03-20 01:49:39 +0000

    Add 15.c

---
+++ src/15.c
@@ -0,0 +1,9 @@
+// A simple C program to calculate the sum of two numbers
+#include <stdio.h>
+int main() {
+    int num1 = 5;
+    int num2 = 10;
+    int sum = num1 + num2;
+    printf("Sum is: %d\n", sum);
+    return 0;
+}
