dda8fc94d0a5d3e83f904cba9862795167f844bc
briseidaujpan81 <briseidaujpan81@gmail.com>
2025-05-23 04:58:41 +0000

    Add 38.c

---
+++ src/38.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int x = 5;
+    int y = 3;
+
+    printf("x + y = %d\n", x + y);
+    printf("x - y = %d\n", x - y);
+    printf("x * y = %d\n", x * y);
+    printf("x / y = %f\n", (float)x / y);
+
+    return 0;
+}
